9849f9a738a101d6bd95f8bd5c83a0dedda11c9e
Brutski93 <abrutski@gmail.com>
2024-09-17 18:27:28 +0300

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+
 module.exports = function reverse (n) {
-  
+  let str = n;
+  if (n < 0) str = -str;
+  str = `${str}`;
+  let answer = '';
+  for (let i = (str.length - 1); i >= 0; i--) {
+    answer += str[i];
+  }
+  return +answer;
 }
+
+const reverse = (n) => {
+  let str = n;
+  if (n < 0) str = -str;
+  str = `${str}`;
+  let answer = '';
+  for (let i = (str.length - 1); i >= 0; i--) {
+    answer += str[i];
+  }
+  return +answer;
+}
+
+/* tests */
+let j = -123;
+console.log(j, ':', reverse(j));
+/* for
+for (let j = 10; j < 123; j++) {
+  console.log(j, ':', reverse(j));
+}
+*/
