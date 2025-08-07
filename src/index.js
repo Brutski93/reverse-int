9260ab6ddeb230b4e1a50b6130cfc6e10735ea8a
Brutski93 <abrutski@gmail.com>
2025-08-07 13:58:40 +0300

    fix: rework for 2025

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const str = `${Math.abs(n)}`;
+  let answer = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    answer += str[i];
+  }
+  return +answer;
 };
